c941b21b6f08bfa30ced74ab1990a15b1f922557
GertMadsen <cph-gm50@cphbusiness.dk>
2018-12-04 11:23:10 +0100

    scrape and download of pictures ready

---
+++ lib/scrape_images.py
@@ -0,0 +1,31 @@
+import bs4
+import os
+import sys
+import requests
+from urllib import request as req
+
+
+def collect_img_links(url):
+    r = requests.get(url)
+    r.raise_for_status()
+    soup = bs4.BeautifulSoup(r.text, 'html5lib')
+    
+    return [img.get('src') for img in soup.select('img') 
+            if img.get('src').startswith('http')]
+
+
+def download(from_url, to_file): 
+    if not os.path.isfile(to_file):
+        try: 
+            req.urlretrieve(from_url, to_file)
+        except:
+            pass    
+
+def download_imgs(links, out_folder="./images/"):
+    for l in links:
+        file_name = os.path.join(out_folder, os.path.basename(l))
+        download(l,file_name)
+
+def scrape(url):
+    links = collect_img_links(url)
+    download_imgs(links)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import lib.scrape_images as scraper
 
 
 if __name__ == '__main__':
-    pass
-    
+    # scraper.scrape('https://commons.wikimedia.org/wiki/Main_Page')
+    scraper.scrape('https://burst.shopify.com/nature')
