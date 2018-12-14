9e16a03ae6e45f64c5f78100a1049cb7ccb077cb
GertMadsen <cph-gm50@cphbusiness.dk>
2018-12-14 10:50:36 +0100

    perceptron og training ændret til 1 dimensionelt array

--- lib/Perceptron.py
+++ lib/Perceptron.py
@@ -2,30 +2,26 @@ import numpy as np
 import glob
 from PIL import Image
 
-imageFolderPath = '../training_images'
-imagePath = glob.glob(imageFolderPath + '/*.JPG') 
+forestFolderPath = '../training_images/forest'
+forestImagePath = glob.glob(forestFolderPath + '/*.JPG') 
+urbanFolderPath = '../training_images/urban'
+urbanImagePath = glob.glob(urbanFolderPath + '/*.JPG') 
 
-im_array = np.array( [np.array(Image.open(img).convert('L'), 'f') for img in imagePath] )
 
-forrest_images = get_image_array(im_array)
-forrest_weights = pla(forrest_images)
+forest_array = np.array( [np.array(Image.open(img).convert('L'), 'f') for img in forestImagePath] )
+urban_array = np.array( [np.array(Image.open(img).convert('L'), 'f') for img in urbanImagePath] )
 
-def perceptron_forrest(input, weights):
+forest_training = get_training_data(forest_array,urban_array)
+forest_weights,_ = pla(forest_training)
+urban_training = get_training_data(urban_array,forest_array)
+urban_weights,_ = pla(urban_training)
+
+
+def perceptron(input, weights):
   # This is the same as the dot product np.dot(i, w)
   dot_product = sum([i * w for i, w in zip(input, weights)])
   output = activate(dot_product)
   return output
-
-
-def predict(inp_vec, weights):
-  """
-  inp_vec:
-  An input vector consisting of sepal length and width
-  return:
-  A class label, either 1 for 'setosa' or -1 for 'other'
-  """
-  class_label = perceptron_forrest(inp_vec, weights)
-  return class_label
 
 def pla(training_data, no_iterations=10000, eta=0.01):
   # eta is the learning rate
@@ -37,12 +33,10 @@ def pla(training_data, no_iterations=10000, eta=0.01):
 
   for i in range(no_iterations):
     inp_vec, expected_label = training_data[i % len(training_data)]
-    perceptron_output = perceptron_forrest(inp_vec, weights)
+    perceptron_output = perceptron(inp_vec, weights)
     error = expected_label - perceptron_output
-    # print(error)
     weights += eta * error * inp_vec
-    weight_history.append(np.copy(weights))
-        
+    weight_history.append(np.copy(weights))        
     return weights, weight_history 
 
 def activate(num):
@@ -50,11 +44,17 @@ def activate(num):
   if num > 0:
      return 1
   return -1
-    
-def get_image_array(img_array):
+
+def reshape(image):
+  h, w, d = image.shape
+  image_lst = image.reshape(h * w, d)
+  image_arr = np.reshape(image_lst, h*w*3)
+  return image_arr
+
+def get_training_data(right_array,wrong_array):
   training_data_list = []
-  for image in img_array:
-    h, w, d = image.shape
-    small_image_lst = image.reshape(h * w, d)
-    training_data_list.append(small_image_lst)
+  for image in right_array:
+    training_data_list.append((reshape(image),1))
+  for image in wrong_array:
+    training_data_list.append((reshape(image),-1))
   return training_data_list
